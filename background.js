f433c42f85814764c341eeba2f1a78c4b6370874
Risa Harada <supikateriburu@gmail.com>
2017-06-28 15:31:42 +0900

    :wrench:

--- background.js
+++ background.js
@@ -1,13 +1,8 @@
-var numbersReload = 0;
 var observer = new MutationObserver(function (MutationRecords, MutationObserver) {
-  numbersReload++;
-  if (numbersReload == 3){
     var currentBoard = document.location.href;
     if(localStorage.getItem(currentBoard)) {
       $('.board, .board-body').css('background-color',localStorage.getItem(currentBoard));
-      console.log(localStorage.getItem(currentBoard));
     }
-  }
 });
 
 observer.observe($('body').get(0), {
@@ -18,7 +13,6 @@ document.addEventListener('Load', function () {
   var currentBoard = document.location.href;
   if(localStorage.getItem(currentBoard)) {
     $('.board, .board-body').css('background-color',localStorage.getItem(currentBoard));
-    console.log(localStorage.getItem(currentBoard));
   }
 });
 
